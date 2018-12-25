190d68b3be340de3d7852f60515cc7d63bcdae3c
Stephen Appiah <eunisaesea@gmal.com>
2018-12-25 14:55:37 +0100

    removed app.listern

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
     if (res.status(404)) res.redirect('/');
 })
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-})
