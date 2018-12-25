44c1e27b31fb246df5aa96f06db77d96964a8cdf
Stephen Appiah <eunisaesea@gmal.com>
2018-12-25 15:13:05 +0100

    added routes

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+
 app.use(function(req, res, next){
-    if (res.status(404)) res.redirect('/');
+    if (res.status(404)) res.redirect("/");
 })
